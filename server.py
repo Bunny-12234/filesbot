7d9d1b9eabb7f12afaf8456a724674856568c50d
Bunny-12234 <hanumasai12@gmail.com>
2025-09-05 16:29:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,11 +2,12 @@ from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
-GAMES_FOLDER = "games"  # folder where files will be stored
 
-@app.route("/<path:filename>")
+@app.route('/<path:filename>')
 def serve_file(filename):
-    return send_from_directory(GAMES_FOLDER, filename)
+    games_dir = os.path.join(os.getcwd(), "games")
+    return send_from_directory(games_dir, filename)
 
 if __name__ == "__main__":
+    os.makedirs("games", exist_ok=True)
     app.run(host="0.0.0.0", port=8080)
